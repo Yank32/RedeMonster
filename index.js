91a38c30a6775c53992402fa453b9163eb8bbc77
Yank32 <67502426+Yank32@users.noreply.github.com>
2020-07-07 10:23:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-const express = require('express');
-const app = express();
-app.get("/", (request, response) => {
-  const ping = new Date();
-  ping.setHours(ping.getHours() - 3);
-  console.log(`Ping recebido às ${ping.getUTCHours()}:${ping.getUTCMinutes()}:${ping.getUTCSeconds()}`);
-  response.sendStatus(200);
-});
-app.listen(process.env.PORT); // Recebe solicitações que o deixa online
+const dotenv = require('dotenv');
+if(process.env.NODE_ENV !== 'production')
+    dotenv.config();
+
+const { CLIENT_TOKEN } = process.env;
 
 const Discord = require("discord.js"); //Conexão com a livraria Discord.js
 const client = new Discord.Client(); //Criação de um novo Client
